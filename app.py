42272b3cc434343ef890b5c9668d9071c359dd40
OliverWong258 <3185336653@qq.com>
2023-12-21 22:25:19 +0800

    fixed app.py

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ from init import create_app
 from views import index, login, logout, register, update_target
 
 
-if __name__ == '__main__':
-    print("Python version: ", sys.version)
-    app = create_app()
-    app.add_url_rule('/', 'index', index)
-    app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
-    app.add_url_rule('/logout', 'logout', logout)
-    app.add_url_rule('/register', 'register', register, methods=['GET', 'POST'])
-    app.add_url_rule('/update_target', 'update_target', update_target, methods=['POST'])
-    app.run(debug=True)
+app = create_app()
+app.add_url_rule('/', 'index', index)
+app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
+app.add_url_rule('/logout', 'logout', logout)
+app.add_url_rule('/register', 'register', register, methods=['GET', 'POST'])
+app.add_url_rule('/update_target', 'update_target', update_target, methods=['POST'])
+app.run(debug=True)
